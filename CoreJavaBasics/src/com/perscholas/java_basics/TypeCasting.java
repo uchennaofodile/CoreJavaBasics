ed0a64bb64f3f99ff5d6f73106b39a5db0cd83dc
Uchenna Ofodile GitHub <uchenna.ofodile@gmail.com>
2023-01-05 21:10:05 -0500

    Deleted files

--- CoreJavaBasics/src/com/perscholas/java_basics/TypeCasting.java
+++
@@ -1,9 +0,0 @@
-package com.perscholas.java_basics;
-
-public class TypeCasting {
-
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    System.out.println("hi");
-  }
-}
